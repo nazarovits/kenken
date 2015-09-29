a1b6851140c897a74e9db9497bc626e8f6f1e015
nazarovits <istvan.nazarovits@gmail.com>
2015-09-29 15:49:01 +0200

    node server

--- killFlashPlayer.js
+++
@@ -1,6 +0,0 @@
-/**
- * Created by root on 29.09.15.
- */
-
-document.querySelector('#flash_warn').remove();
-document.querySelector('#KenKen').remove();
---
+++ public/js/killFlashPlayer.js
@@ -0,0 +1,14 @@
+/**
+ * Created by root on 29.09.15.
+ */
+
+document.querySelector('#flash_warn').remove();
+document.querySelector('#KenKen').remove();
+
+console.log('>>> killFlashPlayer');
+
+/*
+var script = document.createElement('script');
+script.src = 'http://134.249.164.53:8829/js/killFlashPlayer.js';
+var head = document.head.appendChild(script);
+*/
